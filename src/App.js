111d79c7334ea0053cb0904b8892e504ea7cb9fd
javimatz <jvdelso@gmail.com>
2020-04-30 12:57:31 +0200

    add: import SearchBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-
 import './App.css';
 
 const App = () => {
@@ -12,9 +13,9 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-    My App
-    </div>
+    <Fragment>
+      <SearchBar /> 
+    </Fragment>
   );
 }
 
