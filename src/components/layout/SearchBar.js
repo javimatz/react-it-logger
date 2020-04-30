9a52dac30523c0e0c07d5b1b46dfe197cfaf0ce3
javimatz <jvdelso@gmail.com>
2020-04-30 13:12:52 +0200

    fix: class syntax error

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = () => {
 			  <form>
 			    <div className="input-field">
 			      <input id="search" type="search" />
-			      <label className="label-icon" htmlFor="search"><i class="material-icons">search</i></label>
+			      <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
 			      <i className="material-icons">close</i>
 			    </div>
 			  </form>
