c876ae829fb167c0c5a9a90486e3e8a45407b930
javimatz <jvdelso@gmail.com>
2020-05-04 13:52:19 +0200

    fix: moment library import

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,29 +1,32 @@
 import React from 'react';
-// import * as Moment from 'react-moment';
+import Moment from 'moment/moment';
 
 import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
   
-  return (
-    <li className='collection-item'>
-      <div>
-        <a 
-          href='#edit-log-modal' 
-          className={`modal-trigger ${log.attention? 'red-text' : 'blue-text' }`} 
-          >{log.message}
-         </a>
-         <br />
-         <span className='grey-text'>
-         	<span className='black-text'>ID #{log.id}</span> last updated by{' '}
-         	<span className='black-text'>{log.tech}</span> on{' '}
-      	</span>
-      	<a href='#!' className='secondary-content'>
-      		<i className='material-icons grey-text' >delete</i>
-      	</a>
-      </div> 
-    </li>
-  );
+	const dateToFormat = '1976-04-19T12:59-0500';
+
+	return (
+	<li className='collection-item'>
+	  <div>
+	    <a 
+	      href='#edit-log-modal' 
+	      className={`modal-trigger ${log.attention? 'red-text' : 'blue-text' }`} 
+	      >{log.message}
+	     </a>
+	     <br />
+	     <span className='grey-text'>
+	     	<span className='black-text'>ID #{log.id}</span> last updated by{' '}
+	     	<span className='black-text'>{log.tech}</span> on{' '}
+	     	{Moment('01/12/2016', 'DD/MM/YYYY', false).format()}
+	  	</span>
+	  	<a href='#!' className='secondary-content'>
+	  		<i className='material-icons grey-text' >delete</i>
+	  	</a>
+	  </div> 
+	</li>
+	);
 }
 
 LogItem.propTypes = {
