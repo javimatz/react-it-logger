78704a3535a1b871276bc04a0148ecc3f6a9d5ab
javimatz <jvdelso@gmail.com>
2020-05-04 12:31:47 +0200

    add: delete icon

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import Moment from 'react-moment';
+// import * as Moment from 'react-moment';
+
 import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
+  
   return (
     <li className='collection-item'>
       <div>
         <a 
           href='#edit-log-modal' 
           className={`modal-trigger ${log.attention? 'red-text' : 'blue-text' }`} 
-          >{log.message}</a>
+          >{log.message}
+         </a>
+         <br />
+         <span className='grey-text'>
+         	<span className='black-text'>ID #{log.id}</span> last updated by{' '}
+         	<span className='black-text'>{log.tech}</span> on{' '}
+      	</span>
+      	<a href='#!' className='secondary-content'>
+      		<i className='material-icons grey-text' >delete</i>
+      	</a>
       </div> 
     </li>
   );
