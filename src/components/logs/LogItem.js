d93af0100ba53500ca49df8f58fd3287bbd0bf12
javimatz <jvdelso@gmail.com>
2020-05-05 11:56:06 +0200

    fix: Moment format

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -19,7 +19,7 @@ const LogItem = ({ log }) => {
 	     <span className='grey-text'>
 	     	<span className='black-text'>ID #{log.id}</span> last updated by{' '}
 	     	<span className='black-text'>{log.tech}</span> on{' '}
-	     	{Moment(log.date, 'MM/DD/YYYY').format()}
+	     	{Moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}
 	  	</span>
 	  	<a href='#!' className='secondary-content'>
 	  		<i className='material-icons grey-text' >delete</i>
