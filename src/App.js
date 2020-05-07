86667e7b9d6a82da04c1ccfeb67c7a78017ed4b7
javimatz <jvdelso@gmail.com>
2020-05-07 10:16:01 +0200

    add: redux store

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -19,17 +22,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar /> 
-      <div className="container">
-        <AddBtn />
-        <AddTechModal />
-        <AddLogModal />
-        <EditLogModal />
-        <TechListModal />
-      	<Logs />
-      </div>
-    </Fragment>
+    <Provider store={store} >
+      <Fragment>
+        <SearchBar /> 
+        <div className="container">
+          <AddBtn />
+          <AddTechModal />
+          <AddLogModal />
+          <EditLogModal />
+          <TechListModal />
+        	<Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
