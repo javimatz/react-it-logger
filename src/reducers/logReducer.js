a5ad6265bbcddc882e8885ad5033504e58680a9d
javimatz <jvdelso@gmail.com>
2020-05-07 11:07:00 +0200

    add: GET_LOGS, LOGS_ERROR cases

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,4 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
 
 const initialState = {
 	logs: null,
@@ -8,6 +9,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
 	switch(action.type) {
+		case GET_LOGS:
+			return {
+				...state,
+				logs: action.payload,
+				loading: false
+			}
+		case SET_LOADING: 
+			return {
+				...state,
+				loading: true
+			}
+		case LOGS_ERROR:
+		console.log(action.payload)
+			return {
+				...state,
+				error: action.payload
+			}
 		default: 
 			return state;
 	}
