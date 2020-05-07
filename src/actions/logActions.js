2243b496fae9ff8979d20eb3568ed5d3e6eefa35
javimatz <jvdelso@gmail.com>
2020-05-07 13:04:14 +0200

    add: deleteLog, addLog actions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,6 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
 
+// Get logs from server
 export const getLogs = () => async (dispatch) => {
 
 	try {
@@ -20,6 +21,59 @@ export const getLogs = () => async (dispatch) => {
 		});
 	}
 }
+
+// Add new log
+export const addLog = (log) => async (dispatch) => {
+
+	try {
+		setLoading();
+
+		const res = await fetch('/logs', {
+			method: 'POST',
+			body: JSON.stringify(log),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_LOG,
+			payload: data
+		});
+
+	} catch(err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+}
+
+// Delete log
+export const deleteLog = (id) => async (dispatch) => {
+
+	try {
+		setLoading();
+
+		await fetch(`/logs/${id}`, {
+			method: 'DELETE'
+		});
+		
+		dispatch({
+			type: DELETE_LOG,
+			payload: id
+		});
+
+	} catch(err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+}
+
 
 // Set loading to true
 export const setLoading = () => {
