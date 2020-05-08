75d426d469e249401904ffa0093b7f1fc0c24f7a
javimatz <jvdelso@gmail.com>
2020-05-08 12:35:08 +0200

    add: searchLogs action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,7 @@ import {
 	LOGS_ERROR, 
 	ADD_LOG, 
 	UPDATE_LOG,
+	SEARCH_LOGS,
 	DELETE_LOG,
 	SET_CURRENT,
 	CLEAR_CURRENT
@@ -101,6 +102,28 @@ export const updateLog = (log) => async (dispatch) => {
 
 		dispatch({
 			type: UPDATE_LOG,
+			payload: data
+		});
+
+	} catch(err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+}
+
+// Search server logs
+export const searchLogs = (text) => async (dispatch) => {
+
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs?q=${text}`);
+		const data = await res.json();
+
+		dispatch({
+			type: SEARCH_LOGS,
 			payload: data
 		});
 
