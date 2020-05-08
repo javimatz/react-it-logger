626c7c5b9ff0f729a82b3db31335802e06bd4ef4
javimatz <jvdelso@gmail.com>
2020-05-08 13:44:27 +0200

    remove: console.log

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -64,7 +64,6 @@ export default (state = initialState, action) => {
 				loading: true
 			}
 		case LOGS_ERROR:
-		console.log(action.payload)
 			return {
 				...state,
 				error: action.payload
