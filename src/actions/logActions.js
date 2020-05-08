4f827ee44dc108c2808e4acf43709d73814d2937
javimatz <jvdelso@gmail.com>
2020-05-08 10:29:34 +0200

    add: updateLog

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -83,19 +83,48 @@ export const deleteLog = (id) => async (dispatch) => {
 	}
 }
 
+// Update log on server
+export const updateLog = (log) => async (dispatch) => {
+
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs/${log.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(log),
+			headers: {
+				'Content-Type': 'application/JSON'
+			}
+		});
+	
+		const data = await res.JSON();
+
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data
+		});
+
+	} catch(err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+}
+
 // Set current log
 export const setCurrent = (log) => {
-	dispatch({
+	return {
 		type: SET_CURRENT,
 		payload: log
-	});
+	}
 } 
 
 // Clear current log
-export const clearCurrent = (log) => {
-	dispatch({
+export const clearCurrent = () => {
+	return {
 		type: CLEAR_CURRENT
-	});
+	}
 } 
 
 // Set loading to true
